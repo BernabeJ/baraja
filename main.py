b3ba9b1ebd37cd81c209f39abd00aa236bc01b22
Bernabe Jimenez <bernabejimenez@MacBook-Pro-de-Bernabe.local>
2020-04-27 22:14:40 +0200

    creacion modulo

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
+import cartas
 
-
-palos = ('o', 'c', 'e', 'b')
-cartas = ('A', '2', '3', '4', '5', '6', '7', 's', 'c', 'r')
-
-
-def crea_baraja():
-    baraja = []
-    for palo in palos:
-        for carta in cartas:
-            naipe = carta + palo
-            baraja.append(naipe)
-
-    return baraja
+b1 = cartas.crea_baraja()
+b2 = cartas.crea_baraja()
 
