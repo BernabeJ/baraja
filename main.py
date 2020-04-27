97ea8f46df856a15d6d44d7bfe27276ba2cb2256
Bernabe Jimenez <bernabejimenez@MacBook-Pro-de-Bernabe.local>
2020-04-27 21:54:38 +0200

    baraja en funcion

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+
+
 palos = ('o', 'c', 'e', 'b')
 cartas = ('A', '2', '3', '4', '5', '6', '7', 's', 'c', 'r')
-baraja = []
 
-for palo in palos:
-    for carta in cartas:
-        naipe = carta + palo
-        baraja.append(naipe)
-print (baraja)
+
+def crea_baraja():
+    baraja = []
+    for palo in palos:
+        for carta in cartas:
+            naipe = carta + palo
+            baraja.append(naipe)
+
+    return baraja
+
