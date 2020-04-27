e1ec4e1dc7da61ca56d36e150ae649852d069054
Bernabe Jimenez <bernabejimenez@MacBook-Pro-de-Bernabe.local>
2020-04-27 21:30:33 +0200

    Version inicial

---
+++ main.py
@@ -0,0 +1,9 @@
+palos = ('o', 'c', 'e', 'b')
+cartas = ('A', '2', '3', '4', '5', '6', '7', 's', 'c', 'r')
+baraja = []
+
+for palo in palos:
+    for carta in cartas:
+        naipe = carta + palo
+        baraja.append(naipe)
+print (baraja)
